5030e97c508b36c40222e41fb34cb4cf5e4707b5
trofim110 <trofim.chuchuk@mail.ru>
2022-07-06 16:38:29 +0700

    1

---
+++ CsharpProject/Program.cs
@@ -0,0 +1,38 @@
+﻿
+public class Cycler
+{
+    private string[] array;
+    private int index = -1;
+
+    public  Cycler(string[] array)
+    {
+        this.array = array;
+    }
+
+    public  string GetNext()
+    {
+        index++;
+        int size = array.Length;
+        if (size == index)
+        {
+            index = index - size;
+        }
+        return array[index];
+    }
+}
+public class Employee
+{
+    static void Main()
+    {
+        string[] array = { "1", "2", "3" };
+         var cycler = new Cycler(array);
+         Console.WriteLine(cycler.GetNext());
+         Console.WriteLine(cycler.GetNext());
+         Console.WriteLine(cycler.GetNext());
+         Console.WriteLine(cycler.GetNext());
+         Console.WriteLine("-----------------------------------");
+
+    }
+
+}
+
