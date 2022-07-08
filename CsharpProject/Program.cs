5780e0ca08c62756d5de7b0fcbbe515251c1526b
trofim110 <trofim.chuchuk@mail.ru>
2022-07-08 15:31:25 +0700

    Completed the tests

--- CsharpProject/Program.cs
+++ CsharpProject/Program.cs
@@ -2,16 +2,14 @@
 public class Cycler <T>
 {
     private List<T> array;
-    private int _index = -1;
+    private static int _index = -1;
 
     public int Index
     {
         get {  return _index;  }
 
         set
-        {
-            _index = value;
-
+        {_index = value;
             if (value >= array.Count)
                 _index = 0;
         }
@@ -32,30 +30,7 @@ public class Employee
 {
     static void Main()
     {
-        List<int>  listInt = new (){1,2,3};
-        List<string> listString = new() { "s", "q" };
-        List<char> listChar = new() { 'a', 'b' };
 
-        var numbers = new Cycler<int> (listInt);
-        var word = new Cycler<string>(listString);
-        var letter = new Cycler<char>(listChar);
-
-
-
-         Console.WriteLine(numbers .GetNext());
-         Console.WriteLine(numbers .GetNext());
-         Console.WriteLine(numbers .GetNext());
-         Console.WriteLine(numbers .GetNext());
-         Console.WriteLine("-----------------------------------");
-         Console.WriteLine(word.GetNext());
-         Console.WriteLine(word.GetNext());
-         Console.WriteLine(word.GetNext());
-         Console.WriteLine(word.GetNext());
-         Console.WriteLine("-----------------------------------");
-         Console.WriteLine(letter.GetNext());
-         Console.WriteLine(letter.GetNext());
-         Console.WriteLine(letter.GetNext());
-         Console.WriteLine(letter.GetNext());
     }
 
 
--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -10,20 +10,70 @@ public class UnitTest1
     {
         // Arrange
         var list = new List<int>() { 1, 2, 3 };
-        int a = 1;
-        int b = 2;
-        int c = 3;
+        var list1 = 1;
+        var list2 = 2;
+        var list3 = 3;
 
         // Act
 
-        Cycler<int> sss = new Cycler<int>(list);
-        sss.GetNext();
+        var value = new Cycler<int>(list);
+        value.GetNext();
 
         // Assert
-        Equals(sss.GetNext(), a);
-        Equals(sss.GetNext(), b);
-        Equals(sss.GetNext(), c);
-        Equals(sss.GetNext(), a);
+        Equals(value.GetNext(), list1);
+        Equals(value.GetNext(), list2);
+        Equals(value.GetNext(), list3);
+        Equals(value.GetNext(), list1);
+
+    }
+
+}
+
+public class UnitTest2
+{
+    [Fact]
+    public void Test2()
+    {
+        // Arrange
+        var word = new List<string>() { "qq", "ww", "ee" };
+        var word1 = "qq";
+        var word2 = "ww";
+        var word3 = "ee";
+
+        // Act
+
+        var Word = new Cycler<string>(word);
+        Word.GetNext();
+
+        // Assert
+        Equals(Word.GetNext(), word1);
+        Equals(Word.GetNext(), word2);
+        Equals(Word.GetNext(), word3);
+        Equals(Word.GetNext(), word1);
+
+    }
+}
+
+public class UnitTest3
+{
+    [Fact]
+    public void Test3()
+    {
+        // Arrange
+        var list = new List<char>() { 's', 'f', 'g' };
+        var list1 = 's';
+        var list2 = 'f';
+        var list3 = 'g';
+
+        // Act
+        var letter = new Cycler<char>(list);
+        letter.GetNext();
+
+        // Assert
+        Equals(letter.GetNext(), list1);
+        Equals(letter.GetNext(), list2);
+        Equals(letter.GetNext(), list3);
+        Equals(letter.GetNext(), list1);
 
     }
 }
